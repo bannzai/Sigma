64a019dba8ed141ac085330823006e075e0994ce
bannzai <kingkong999yhirose@gmail.com>
2021-11-20 10:58:32 +0900

    Fix entrypoint

--- code.ts
+++ code.ts
@@ -1,21 +1,20 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5;
+import { walkToScene } from "./src/swiftui/walkers/walkers";
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
+const run = () => {
+  const root = figma.currentPage.selection[0];
+  const traversedContext = traversed(root);
+  print(traversedContext);
+};
 
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
-}
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
+const traversed = (root: SceneNode): SwiftUIContext => {
+  const context = new SwiftUIContext();
+  walkToScene(context, root);
+  return context;
+};
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
+const print = (context: SwiftUIContext) => {
+  console.log(context.code.length);
+};
+
+run();
 figma.closePlugin();
