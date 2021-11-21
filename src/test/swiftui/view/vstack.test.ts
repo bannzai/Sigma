469b2b1e55253573ff3fb0ae2481fa9ccb47985c
bannzai <kingkong999yhirose@gmail.com>
2021-11-21 23:59:33 +0900

    :sparkels: add test case for padding to vstack

--- src/test/swiftui/view/vstack.test.ts
+++ src/test/swiftui/view/vstack.test.ts
@@ -37,4 +37,32 @@ describe("#VStack", () => {
     }`;
     expect(context.code).toEqual(code.slice("\n".length));
   });
+
+  test("VStack with padding", async () => {
+    await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+    const vstack = figma.createFrame();
+    vstack.layoutMode = "VERTICAL";
+    vstack.counterAxisAlignItems = "MIN";
+    vstack.paddingLeft = 20;
+    vstack.paddingTop = 20;
+    vstack.paddingRight = 20;
+    vstack.paddingBottom = 20;
+    vstack.itemSpacing = 10;
+    vstack.appendChild(createText("1"));
+    vstack.appendChild(createText("2"));
+    vstack.appendChild(createText("3"));
+
+    const context = new SwiftUIContext();
+    walk(context, vstack);
+
+    const code = `
+    VStack(alignment: .leading, spacing: 10) {
+        Text("1")
+        Text("2")
+        Text("3")
+    }
+    .padding(.all, 20)`;
+    expect(context.code).toEqual(code.slice("\n".length));
+  });
 });
