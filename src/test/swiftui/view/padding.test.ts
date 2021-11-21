9967f324d8014aea3bf3c89e358614462d96032b
bannzai <kingkong999yhirose@gmail.com>
2021-11-22 00:10:04 +0900

    :sparkles: add padding test case

--- src/test/swiftui/view/padding.test.ts
+++ src/test/swiftui/view/padding.test.ts
@@ -122,5 +122,33 @@ describe("#View.padding(_:)", () => {
     .padding([.left, .top], 10)`;
       expect(context.code).toEqual(code.slice("\n".length));
     });
+
+    test("When specify same value for top and bottom", async () => {
+      await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+      const vstack = figma.createFrame();
+      vstack.layoutMode = "VERTICAL";
+      vstack.counterAxisAlignItems = "MIN";
+      vstack.paddingLeft = 0;
+      vstack.paddingTop = 10;
+      vstack.paddingRight = 0;
+      vstack.paddingBottom = 10;
+      vstack.itemSpacing = 10;
+      vstack.appendChild(createText("1"));
+      vstack.appendChild(createText("2"));
+      vstack.appendChild(createText("3"));
+
+      const context = new SwiftUIContext();
+      walk(context, vstack);
+
+      const code = `
+    VStack(alignment: .leading, spacing: 10) {
+        Text("1")
+        Text("2")
+        Text("3")
+    }
+    .padding(.vertical, 10)`;
+      expect(context.code).toEqual(code.slice("\n".length));
+    });
   });
 });
