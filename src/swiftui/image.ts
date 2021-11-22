3f6498f74025e0c07d3a5634ace25e8bca1890be
bannzai <kingkong999yhirose@gmail.com>
2021-11-22 12:43:46 +0900

    :memo: missing path

--- src/swiftui/image.ts
+++ src/swiftui/image.ts
@@ -1,9 +1,8 @@
 import { SwiftUIContext } from "./context";
 
-export function walkForImage(
-  context: SwiftUIContext,
-  node: MinimalFillsMixin & BaseStyle
-) {
+export type ImageNode = MinimalFillsMixin & BaseNode;
+
+export function walkForImage(context: SwiftUIContext, node: ImageNode) {
   const { name, fills } = node;
   if (fills !== figma.mixed) {
     for (const fill of fills) {
