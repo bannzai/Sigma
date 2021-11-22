06ece29721c9b451f89eb603102b8b765ae6d8ca
bannzai <kingkong999yhirose@gmail.com>
2021-11-22 14:57:18 +0900

    :sparkles: add blend mixin type guard

---
+++ src/swiftui/type/type_guards.ts
@@ -0,0 +1,20 @@
+export function isBlendMixin(node: any): node is BlendMixin {
+  const blendMixinTypes = [
+    "ELLIPSE",
+    "POLYGON",
+    "STAR",
+    "VECTOR",
+    "TEXT",
+    "FRAME",
+    "COMPONENT",
+    "INSTANCE",
+    "COMPONENT_SET",
+    "GROUP",
+  ];
+
+  if (blendMixinTypes.includes(node.type)) {
+    return true;
+  } else {
+    return false;
+  }
+}
