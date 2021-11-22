3cc126b36fa88c2f6f4a8090b3dd1287b4bb79f5
bannzai <kingkong999yhirose@gmail.com>
2021-11-22 10:16:16 +0900

    :sparkles: add test case for Button

---
+++ src/test/swiftui/view/button.test.ts
@@ -0,0 +1,30 @@
+import { SwiftUIContext } from "../../../swiftui/context";
+import { walk } from "../../../swiftui/walkers/walkers";
+import { createText } from "../utility/utility";
+import { createFigma } from "figma-api-stub";
+
+describe("#Button", () => {
+  const figma = createFigma({
+    simulateErrors: true,
+    isWithoutTimeout: false,
+  });
+  // @ts-ignore for some reason, need to override this for figma.mixed to work
+  global.figma = figma;
+
+  test("it is besically pattern. For Text Button", async () => {
+    await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+    const root = figma.createFrame();
+    const button = figma.group([createText("1")], root);
+    button.name = "SwiftUI:Button";
+
+    const context = new SwiftUIContext();
+    walk(context, button);
+
+    const code = `
+    Button(action: { /* TODO */ }) {
+        Text("1")
+    }`;
+    expect(context.code).toEqual(code.slice("\n".length));
+  });
+});
