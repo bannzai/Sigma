f7aff0a9b55def32571c71dc372d7f1e86733891
bannzai <kingkong999yhirose@gmail.com>
2021-11-23 07:24:30 +0900

    :sparkels: add test case for HStack

--- src/test/swiftui/modifiers/frame.test.ts
+++ src/test/swiftui/modifiers/frame.test.ts
@@ -214,4 +214,208 @@ VStack(alignment: .leading, spacing: 10) {
       });
     });
   });
+
+  describe("for HStack", () => {
+    describe("HStack layout align for INHERIT mode", () => {
+      test("HStack primary and counter axis size are FIXED", async () => {
+        await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+        const vstack = figma.createFrame();
+        vstack.layoutMode = "HORIZONTAL";
+        vstack.layoutAlign = "INHERIT";
+        vstack.primaryAxisSizingMode = "FIXED";
+        vstack.counterAxisSizingMode = "FIXED";
+        vstack.counterAxisAlignItems = "MIN";
+        vstack.paddingLeft = 0;
+        vstack.paddingTop = 0;
+        vstack.paddingRight = 0;
+        vstack.paddingBottom = 0;
+        vstack.itemSpacing = 10;
+        vstack.resize(100, 200);
+        vstack.appendChild(createText("1"));
+        vstack.appendChild(createText("2"));
+        vstack.appendChild(createText("3"));
+
+        const context = new SwiftUIContext();
+        walk(context, vstack);
+
+        const code = `
+HStack(alignment: .top, spacing: 10) {
+    Text("1")
+    Text("2")
+    Text("3")
+}
+.frame(width: 100, height: 200)
+`;
+        expect(context.code).toEqual(code.slice("\n".length));
+      });
+
+      test("HStack primary axis is FIXED, and counter axis is AUTO", async () => {
+        await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+        const vstack = figma.createFrame();
+        vstack.layoutMode = "HORIZONTAL";
+        vstack.layoutAlign = "INHERIT";
+        vstack.primaryAxisSizingMode = "FIXED";
+        vstack.counterAxisSizingMode = "AUTO";
+        vstack.counterAxisAlignItems = "MIN";
+        vstack.paddingLeft = 0;
+        vstack.paddingTop = 0;
+        vstack.paddingRight = 0;
+        vstack.paddingBottom = 0;
+        vstack.itemSpacing = 10;
+        vstack.resize(100, 200);
+        vstack.appendChild(createText("1"));
+        vstack.appendChild(createText("2"));
+        vstack.appendChild(createText("3"));
+
+        const context = new SwiftUIContext();
+        walk(context, vstack);
+
+        const code = `
+HStack(alignment: .top, spacing: 10) {
+    Text("1")
+    Text("2")
+    Text("3")
+}
+.frame(width: 100)
+`;
+        expect(context.code).toEqual(code.slice("\n".length));
+      });
+
+      test("HStack primary axis is AUTO, and counter axis is FIXED", async () => {
+        await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+        const vstack = figma.createFrame();
+        vstack.layoutMode = "HORIZONTAL";
+        vstack.layoutAlign = "INHERIT";
+        vstack.primaryAxisSizingMode = "AUTO";
+        vstack.counterAxisSizingMode = "FIXED";
+        vstack.counterAxisAlignItems = "MIN";
+        vstack.paddingLeft = 0;
+        vstack.paddingTop = 0;
+        vstack.paddingRight = 0;
+        vstack.paddingBottom = 0;
+        vstack.itemSpacing = 10;
+        vstack.resize(100, 200);
+        vstack.appendChild(createText("1"));
+        vstack.appendChild(createText("2"));
+        vstack.appendChild(createText("3"));
+
+        const context = new SwiftUIContext();
+        walk(context, vstack);
+
+        const code = `
+HStack(alignment: .top, spacing: 10) {
+    Text("1")
+    Text("2")
+    Text("3")
+}
+.frame(height: 200)
+`;
+        expect(context.code).toEqual(code.slice("\n".length));
+      });
+    });
+
+    describe("HStack layout align for STRETCH mode", () => {
+      test("HStack primary and counter axis size are FIXED", async () => {
+        await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+        const vstack = figma.createFrame();
+        vstack.layoutMode = "HORIZONTAL";
+        vstack.layoutAlign = "STRETCH";
+        vstack.primaryAxisSizingMode = "FIXED";
+        vstack.counterAxisSizingMode = "FIXED";
+        vstack.counterAxisAlignItems = "MIN";
+        vstack.paddingLeft = 0;
+        vstack.paddingTop = 0;
+        vstack.paddingRight = 0;
+        vstack.paddingBottom = 0;
+        vstack.itemSpacing = 10;
+        vstack.resize(100, 200);
+        vstack.appendChild(createText("1"));
+        vstack.appendChild(createText("2"));
+        vstack.appendChild(createText("3"));
+
+        const context = new SwiftUIContext();
+        walk(context, vstack);
+
+        const code = `
+HStack(alignment: .top, spacing: 10) {
+    Text("1")
+    Text("2")
+    Text("3")
+}
+.frame(maxWidth: .infinity, maxHeight: .infinity)
+`;
+        expect(context.code).toEqual(code.slice("\n".length));
+      });
+
+      test("HStack primary axis is FIXED, and counter axis is AUTO", async () => {
+        await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+        const vstack = figma.createFrame();
+        vstack.layoutMode = "HORIZONTAL";
+        vstack.layoutAlign = "STRETCH";
+        vstack.primaryAxisSizingMode = "FIXED";
+        vstack.counterAxisSizingMode = "AUTO";
+        vstack.counterAxisAlignItems = "MIN";
+        vstack.paddingLeft = 0;
+        vstack.paddingTop = 0;
+        vstack.paddingRight = 0;
+        vstack.paddingBottom = 0;
+        vstack.itemSpacing = 10;
+        vstack.resize(100, 200);
+        vstack.appendChild(createText("1"));
+        vstack.appendChild(createText("2"));
+        vstack.appendChild(createText("3"));
+
+        const context = new SwiftUIContext();
+        walk(context, vstack);
+
+        const code = `
+HStack(alignment: .top, spacing: 10) {
+    Text("1")
+    Text("2")
+    Text("3")
+}
+.frame(maxWidth: 100, maxHeight: .infinity)
+`;
+        expect(context.code).toEqual(code.slice("\n".length));
+      });
+
+      test("HStack primary axis is AUTO, and counter axis is FIXED", async () => {
+        await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+        const vstack = figma.createFrame();
+        vstack.layoutMode = "HORIZONTAL";
+        vstack.layoutAlign = "STRETCH";
+        vstack.primaryAxisSizingMode = "AUTO";
+        vstack.counterAxisSizingMode = "FIXED";
+        vstack.counterAxisAlignItems = "MIN";
+        vstack.paddingLeft = 0;
+        vstack.paddingTop = 0;
+        vstack.paddingRight = 0;
+        vstack.paddingBottom = 0;
+        vstack.itemSpacing = 10;
+        vstack.resize(100, 200);
+        vstack.appendChild(createText("1"));
+        vstack.appendChild(createText("2"));
+        vstack.appendChild(createText("3"));
+
+        const context = new SwiftUIContext();
+        walk(context, vstack);
+
+        const code = `
+HStack(alignment: .top, spacing: 10) {
+    Text("1")
+    Text("2")
+    Text("3")
+}
+.frame(maxHeight: .infinity)
+`;
+        expect(context.code).toEqual(code.slice("\n".length));
+      });
+    });
+  });
 });
