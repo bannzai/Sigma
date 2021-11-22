d92359da6c11a3c557f330c247a883760e2095d6
bannzai <kingkong999yhirose@gmail.com>
2021-11-22 23:01:46 +0900

    :sparkels: fix test case

--- src/swiftui/modifiers/frame.ts
+++ src/swiftui/modifiers/frame.ts
@@ -39,15 +39,61 @@ export function walkForFrame(
     Document: https://www.figma.com/plugin-docs/api/properties/nodes-layoutalign/
    */
     return;
-  } else if (layoutAlign === "INHERIT") {
-    return;
-  } else if (layoutAlign === "STRETCH") {
-    if (layoutMode === "VERTICAL") {
-      context.add(`.frame(maxWidth: .infinity)`);
-    } else if (layoutMode === "HORIZONTAL") {
-      context.add(`.frame(maxHeight: .infinity)`);
-    }
   } else {
-    const _: never = layoutAlign;
+    if (layoutAlign === "INHERIT") {
+      if (layoutMode === "VERTICAL") {
+        const isFixedHeight = primaryAxisSizingMode !== "AUTO";
+        const isFixedWidth = counterAxisSizingMode !== "AUTO";
+        if (isFixedWidth && isFixedHeight) {
+          context.add(`.frame(width: ${width}, height: ${height})`);
+        } else {
+          if (isFixedWidth) {
+            context.add(`.frame(width: ${width})`);
+          } else if (isFixedHeight) {
+            context.add(`.frame(height: ${height})`);
+          }
+        }
+      } else {
+        const isFixedWidth = primaryAxisSizingMode !== "AUTO";
+        const isFixedHeight = counterAxisSizingMode !== "AUTO";
+        if (isFixedWidth && isFixedHeight) {
+          context.add(`.frame(width: ${width}, height: ${height})`);
+        } else {
+          if (isFixedWidth) {
+            context.add(`.frame(width: ${width})`);
+          } else if (isFixedHeight) {
+            context.add(`.frame(height: ${height})`);
+          }
+        }
+      }
+    } else if (layoutAlign === "STRETCH") {
+      if (
+        primaryAxisSizingMode === "FIXED" &&
+        counterAxisSizingMode === "FIXED"
+      ) {
+        /*
+           NOTE: If the current node is an auto layout frame (e.g. an auto layout frame inside a parent auto layout frame) 
+           if you set layoutAlign to “STRETCH” you should set the corresponding axis – either primaryAxisSizingMode or counterAxisSizingMode – 
+           to be “FIXED”. This is because an auto-layout frame cannot simultaneously stretch to fill its parent and shrink to hug its children.
+           */
+        context.add(`.frame(maxWidth: .infinity, maxHeight: .infinity)`);
+      } else if (layoutMode === "VERTICAL") {
+        const isFixedHeight = primaryAxisSizingMode !== "AUTO";
+        if (isFixedHeight) {
+          context.add(`.frame(maxWidth: .infinity, maxHeight: ${height})`);
+        } else {
+          context.add(`.frame(maxWidth: .infinity)`);
+        }
+      } else if (layoutMode === "HORIZONTAL") {
+        const isFixedWidth = primaryAxisSizingMode !== "AUTO";
+        if (isFixedWidth) {
+          context.add(`.frame(maxWidth: ${width}, maxHeight: .infinity)`);
+        } else {
+          context.add(`.frame(maxHeight: .infinity)`);
+        }
+      }
+    } else {
+      const _: never = layoutAlign;
+    }
   }
 }
