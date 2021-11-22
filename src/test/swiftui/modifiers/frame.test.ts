a88f8726a2bc914c7159b02b7b08887ccd211587
bannzai <kingkong999yhirose@gmail.com>
2021-11-23 07:11:29 +0900

    :sparkels: add frame test case

---
+++ src/test/swiftui/modifiers/frame.test.ts
@@ -0,0 +1,50 @@
+import { SwiftUIContext } from "../../../swiftui/context";
+import { walk } from "../../../swiftui/walkers/walkers";
+import { createText } from "../utility/utility";
+import { createFigma } from "figma-api-stub";
+
+describe("#View.frame(_:)", () => {
+  const figma = createFigma({
+    simulateErrors: true,
+    isWithoutTimeout: false,
+  });
+  // @ts-ignore for some reason, need to override this for figma.mixed to work
+  global.figma = figma;
+
+  describe("for VStack", () => {
+    describe("VStack layout align for INHERIT mode", () => {
+      test("", async () => {
+        await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+        const vstack = figma.createFrame();
+        vstack.layoutMode = "VERTICAL";
+        vstack.layoutAlign = "INHERIT";
+        vstack.primaryAxisSizingMode = "FIXED";
+        vstack.counterAxisSizingMode = "FIXED";
+        vstack.counterAxisAlignItems = "MIN";
+        vstack.paddingLeft = 0;
+        vstack.paddingTop = 0;
+        vstack.paddingRight = 0;
+        vstack.paddingBottom = 0;
+        vstack.itemSpacing = 10;
+        vstack.resize(100, 200);
+        vstack.appendChild(createText("1"));
+        vstack.appendChild(createText("2"));
+        vstack.appendChild(createText("3"));
+
+        const context = new SwiftUIContext();
+        walk(context, vstack);
+
+        const code = `
+VStack(alignment: .leading, spacing: 10) {
+    Text("1")
+    Text("2")
+    Text("3")
+}
+.frame(width: 100, height: 200)
+`;
+        expect(context.code).toEqual(code.slice("\n".length));
+      });
+    });
+  });
+});
