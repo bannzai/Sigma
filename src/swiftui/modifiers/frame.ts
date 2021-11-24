6f56c416bacc85a3a662e8ea91120de494209c87
bannzai <kingkong999yhirose@gmail.com>
2021-11-24 22:25:14 +0900

    :recycle: fix stretch relations

--- src/swiftui/modifiers/frame.ts
+++ src/swiftui/modifiers/frame.ts
@@ -90,15 +90,34 @@ export function walkForFrame(
           context.add(`.frame(maxWidth: .infinity, maxHeight: .infinity)\n`);
         }
       } else {
+        /*
+NOTE: If the current node is an auto layout frame (e.g. an auto layout frame inside a parent auto layout frame) 
+if you set layoutAlign to “STRETCH” you should set the corresponding axis – either primaryAxisSizingMode or counterAxisSizingMode 
+– to be“FIXED”. This is because an auto-layout frame cannot simultaneously stretch to fill its parent and shrink to hug its children.
+
+Document: https://www.figma.com/plugin-docs/api/properties/nodes-layoutalign/
+
+So, (primary|counter)AxisSizingMode === FIXED means the corresponding one is STRETCH
+        */
         if (layoutMode === "VERTICAL") {
-          const isFixedHeight = primaryAxisSizingMode === "FIXED";
-          if (isFixedHeight) {
-            context.add(`.frame(maxWidth: .infinity, maxHeight: ${height})\n`);
+          const isVerticalAxisStretch = primaryAxisSizingMode === "FIXED";
+          const isHorizontalAxisStretch = counterAxisSizingMode === "FIXED";
+          if (isVerticalAxisStretch) {
+            context.add(`.frame(maxHeight: .infinity)\n`);
+          } else if (isHorizontalAxisStretch) {
+            context.add(`.frame(maxWidth: .infinity)\n`);
+          } else if (isVerticalAxisStretch && isHorizontalAxisStretch) {
+            assert(false, "unknown pattern");
           }
         } else if (layoutMode === "HORIZONTAL") {
-          const isFixedWidth = primaryAxisSizingMode === "FIXED";
-          if (isFixedWidth) {
-            context.add(`.frame(maxWidth: ${width}, maxHeight: .infinity)\n`);
+          const isHorizontalAxisStretch = primaryAxisSizingMode === "FIXED";
+          const isVerticalAxisStretch = counterAxisSizingMode === "FIXED";
+          if (isHorizontalAxisStretch) {
+            context.add(`.frame(maxWidth: .infinity)\n`);
+          } else if (isVerticalAxisStretch) {
+            context.add(`.frame(maxHeight: .infinity)\n`);
+          } else if (isVerticalAxisStretch && isHorizontalAxisStretch) {
+            assert(false, "unknown pattern");
           }
         }
       }
