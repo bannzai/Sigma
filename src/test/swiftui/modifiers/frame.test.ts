4e5f833a0bf1c078944af645bf6ba383879ef3d8
bannzai <kingkong999yhirose@gmail.com>
2021-11-27 10:02:01 +0900

    :sparkles: add test case for paret is HStack

--- src/test/swiftui/modifiers/frame.test.ts
+++ src/test/swiftui/modifiers/frame.test.ts
@@ -236,6 +236,230 @@ VStack(alignment: .leading, spacing: 10) {
         });
       });
     });
+    describe("with HStack parent", () => {
+      describe("case for primary axis layout grow", () => {
+        describe("layoutGrow is 0", () => {
+          test("child VStack primary axis size is AUTO and counter axis size is FIXED", async () => {
+            await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+            const vstack = figma.createFrame();
+            vstack.layoutMode = "VERTICAL";
+            vstack.primaryAxisSizingMode = "AUTO"; // Avoid to add `.frame(width:) to child VStack
+            vstack.counterAxisSizingMode = "FIXED";
+            vstack.counterAxisAlignItems = "MIN";
+            vstack.paddingLeft = 0;
+            vstack.paddingTop = 0;
+            vstack.paddingRight = 0;
+            vstack.paddingBottom = 0;
+            vstack.itemSpacing = 10;
+            vstack.resize(100, 200);
+            vstack.appendChild(createText("1"));
+            vstack.appendChild(createText("2"));
+            vstack.appendChild(createText("3"));
+            vstack.layoutGrow = 0;
+
+            const parent = figma.createFrame();
+            parent.layoutMode = "HORIZONTAL";
+            // Any values: BEGIN
+            parent.primaryAxisSizingMode = "FIXED";
+            parent.counterAxisSizingMode = "FIXED";
+            parent.counterAxisAlignItems = "MIN";
+            parent.paddingLeft = 0;
+            parent.paddingTop = 0;
+            parent.paddingRight = 0;
+            parent.paddingBottom = 0;
+            parent.itemSpacing = 10;
+            parent.resize(300, 400);
+            // Any values: END
+            parent.appendChild(vstack);
+            parent.appendChild(createText("4"));
+
+            const context = new SwiftUIContext();
+            walk(context, parent);
+
+            const code = `
+HStack(alignment: .top, spacing: 10) {
+    VStack(alignment: .leading, spacing: 10) {
+        Text(verbatim: "1")
+        Text(verbatim: "2")
+        Text(verbatim: "3")
+    }
+    .frame(width: 100)
+    Text(verbatim: "4")
+}
+.frame(width: 300, height: 400)
+`;
+            expect(context.code).toEqual(code.slice("\n".length));
+          });
+        });
+        describe("layoutGrow is 1", () => {
+          test("child VStack primary axis size is AUTO and counter axis size is FIXED", async () => {
+            await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+            const vstack = figma.createFrame();
+            vstack.layoutMode = "VERTICAL";
+            vstack.primaryAxisSizingMode = "AUTO"; // Avoid to add .frame(width:) to child VStack
+            vstack.counterAxisSizingMode = "FIXED";
+            vstack.counterAxisAlignItems = "MIN";
+            vstack.paddingLeft = 0;
+            vstack.paddingTop = 0;
+            vstack.paddingRight = 0;
+            vstack.paddingBottom = 0;
+            vstack.itemSpacing = 10;
+            vstack.resize(100, 200);
+            vstack.appendChild(createText("1"));
+            vstack.appendChild(createText("2"));
+            vstack.appendChild(createText("3"));
+            vstack.layoutGrow = 1;
+
+            const parent = figma.createFrame();
+            parent.layoutMode = "HORIZONTAL";
+            // Any values: BEGIN
+            parent.primaryAxisSizingMode = "FIXED";
+            parent.counterAxisSizingMode = "FIXED";
+            parent.counterAxisAlignItems = "MIN";
+            parent.paddingLeft = 0;
+            parent.paddingTop = 0;
+            parent.paddingRight = 0;
+            parent.paddingBottom = 0;
+            parent.itemSpacing = 10;
+            parent.resize(300, 400);
+            // Any values: END
+            parent.appendChild(vstack);
+            parent.appendChild(createText("4"));
+
+            const context = new SwiftUIContext();
+            walk(context, parent);
+
+            const code = `
+HStack(alignment: .top, spacing: 10) {
+    VStack(alignment: .leading, spacing: 10) {
+        Text(verbatim: "1")
+        Text(verbatim: "2")
+        Text(verbatim: "3")
+    }
+    .frame(maxWidth: .infinity)
+    Text(verbatim: "4")
+}
+.frame(width: 300, height: 400)
+`;
+            expect(context.code).toEqual(code.slice("\n".length));
+          });
+        });
+      });
+      describe("case for counter axis stretch patern", () => {
+        describe("layoutAlign is INHERIT", () => {
+          test("child VStack primary axis size is FIXED and counter axis size is AUTO", async () => {
+            await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+            const vstack = figma.createFrame();
+            vstack.layoutMode = "VERTICAL";
+            vstack.primaryAxisSizingMode = "FIXED";
+            vstack.counterAxisSizingMode = "AUTO"; // Avoid to add `.frame(width:) to child VStack
+            vstack.counterAxisAlignItems = "MIN";
+            vstack.paddingLeft = 0;
+            vstack.paddingTop = 0;
+            vstack.paddingRight = 0;
+            vstack.paddingBottom = 0;
+            vstack.itemSpacing = 10;
+            vstack.resize(100, 200);
+            vstack.appendChild(createText("1"));
+            vstack.appendChild(createText("2"));
+            vstack.appendChild(createText("3"));
+            vstack.layoutGrow = 0;
+            vstack.layoutAlign = "INHERIT";
+
+            const parent = figma.createFrame();
+            parent.layoutMode = "HORIZONTAL";
+            // Any values: BEGIN
+            parent.primaryAxisSizingMode = "FIXED";
+            parent.counterAxisSizingMode = "FIXED";
+            parent.counterAxisAlignItems = "MIN";
+            parent.paddingLeft = 0;
+            parent.paddingTop = 0;
+            parent.paddingRight = 0;
+            parent.paddingBottom = 0;
+            parent.itemSpacing = 10;
+            parent.resize(300, 400);
+            // Any values: END
+            parent.appendChild(vstack);
+            parent.appendChild(createText("4"));
+
+            const context = new SwiftUIContext();
+            walk(context, parent);
+
+            const code = `
+HStack(alignment: .top, spacing: 10) {
+    VStack(alignment: .leading, spacing: 10) {
+        Text(verbatim: "1")
+        Text(verbatim: "2")
+        Text(verbatim: "3")
+    }
+    .frame(height: 200)
+    Text(verbatim: "4")
+}
+.frame(width: 300, height: 400)
+`;
+            expect(context.code).toEqual(code.slice("\n".length));
+          });
+        });
+        describe("layoutAlign is STRETCH", () => {
+          test("child VStack primary axis size is FIXED and counter axis size is AUTO", async () => {
+            await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+
+            const vstack = figma.createFrame();
+            vstack.layoutMode = "VERTICAL";
+            vstack.primaryAxisSizingMode = "AUTO"; // Avoid to add `.frame(height:) to child VStack
+            vstack.counterAxisSizingMode = "AUTO"; // Avoid to add `.frame(width:) to child VStack
+            vstack.counterAxisAlignItems = "MIN";
+            vstack.paddingLeft = 0;
+            vstack.paddingTop = 0;
+            vstack.paddingRight = 0;
+            vstack.paddingBottom = 0;
+            vstack.itemSpacing = 10;
+            vstack.resize(100, 200);
+            vstack.appendChild(createText("1"));
+            vstack.appendChild(createText("2"));
+            vstack.appendChild(createText("3"));
+            vstack.layoutGrow = 0;
+            vstack.layoutAlign = "STRETCH";
+
+            const parent = figma.createFrame();
+            parent.layoutMode = "HORIZONTAL";
+            // Any values: BEGIN
+            parent.primaryAxisSizingMode = "FIXED";
+            parent.counterAxisSizingMode = "FIXED";
+            parent.counterAxisAlignItems = "MIN";
+            parent.paddingLeft = 0;
+            parent.paddingTop = 0;
+            parent.paddingRight = 0;
+            parent.paddingBottom = 0;
+            parent.itemSpacing = 10;
+            parent.resize(300, 400);
+            // Any values: END
+            parent.appendChild(vstack);
+            parent.appendChild(createText("4"));
+
+            const context = new SwiftUIContext();
+            walk(context, parent);
+
+            const code = `
+HStack(alignment: .top, spacing: 10) {
+    VStack(alignment: .leading, spacing: 10) {
+        Text(verbatim: "1")
+        Text(verbatim: "2")
+        Text(verbatim: "3")
+    }
+    .frame(maxHeight: .infinity)
+    Text(verbatim: "4")
+}
+.frame(width: 300, height: 400)
+`;
+            expect(context.code).toEqual(code.slice("\n".length));
+          });
+        });
+      });
+    });
     describe("without parent", () => {
       test("VStack primary axis size is FIXED and counter axis size is FIXED", async () => {
         await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
