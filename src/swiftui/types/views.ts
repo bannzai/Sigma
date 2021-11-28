3319e0f0ff1568d3d27d97060535f2578a060a01
bannzai <kingkong999yhirose@gmail.com>
2021-11-28 13:21:25 +0900

    :sparkles: defined base views

--- src/swiftui/types/views.ts
+++ src/swiftui/types/views.ts
@@ -1,6 +1,38 @@
 import { Modifier } from "./modifiers";
 
 export interface View {
-  readonly id: string;
+  readonly name: string;
   modifiers: Modifier[];
+}
+
+interface ChildrenMixin {
+  children: View[];
+}
+
+interface AxisMixin {
+  readonly axis: "V" | "H" | "Z";
+}
+
+export interface VStack extends View, ChildrenMixin, AxisMixin {
+  readonly name: "VStack";
+  readonly axis: "V";
+}
+
+export interface HStack extends View, ChildrenMixin, AxisMixin {
+  readonly name: "HStack";
+  readonly axis: "H";
+}
+
+export interface ZStack extends View, ChildrenMixin, AxisMixin {
+  readonly name: "ZStack";
+  readonly axis: "Z";
+}
+
+export interface Button extends View, ChildrenMixin {
+  readonly name: "Button";
+}
+
+export interface Text {
+  readonly name: "Text";
+  readonly text: string;
 }
