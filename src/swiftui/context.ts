eb1635e9f5d4d67c5e88b44e5a174e14fe631d74
bannzai <kingkong999yhirose@gmail.com>
2021-11-28 14:22:52 +0900

    :recycle: remove unnecessary properties

--- src/swiftui/context.ts
+++ src/swiftui/context.ts
@@ -1,43 +1,10 @@
 import { Modifier } from "./types/modifiers";
 import { ContainerMixin, View } from "./types/views";
 
-export interface SwiftUIFrameNode {
-  node: FrameNode;
-  isOnlyOneChild: boolean;
-}
-
 export class SwiftUIContext {
-  indent: number = 0;
-  code: string = "";
-  frameNodeHistories: SwiftUIFrameNode[] = [];
-  ignoredIndent: boolean = false;
-  root!: SceneNode;
   // TODO: Rename to root
   rootView!: View & ContainerMixin;
   containerHistories: (View & ContainerMixin)[] = [];
-
-  nest() {
-    this.indent += 4;
-  }
-  unnest() {
-    this.indent -= 4;
-  }
-
-  push(node: FrameNode) {
-    this.frameNodeHistories.push({
-      node,
-      isOnlyOneChild: node.children.length === 1,
-    });
-  }
-  pop(): SwiftUIFrameNode | null {
-    return this.frameNodeHistories.pop() ?? null;
-  }
-  get latestFrameNode(): SwiftUIFrameNode | null {
-    if (this.frameNodeHistories.length === 0) {
-      return null;
-    }
-    return this.frameNodeHistories[this.frameNodeHistories.length - 1];
-  }
 
   // TODO: Rename to add(view:)
   view(view: View) {}
@@ -55,35 +22,5 @@ export class SwiftUIContext {
   }
   popContainer(): (View & ContainerMixin) | null {
     return this.containerHistories.pop() ?? null;
-  }
-
-  add(
-    code: string,
-    options?: {
-      withoutIndent?: boolean;
-    }
-  ) {
-    var withoutIndent = options?.withoutIndent ?? false;
-    if (code === "\n") {
-      withoutIndent = true;
-    }
-
-    const indent = withoutIndent ? "" : this._indent();
-    this.code += `${indent}${code}`;
-  }
-
-  lineBreak(isForceLineBreak: boolean = false) {
-    if (isForceLineBreak) {
-      this.add("\n");
-    } else if (this.code.length > 0 && !this.code.endsWith("\n")) {
-      this.add("\n");
-    }
-  }
-
-  _indent(): string {
-    if (this.ignoredIndent) {
-      return "";
-    }
-    return Array(this.indent).fill(" ").join("");
   }
 }
