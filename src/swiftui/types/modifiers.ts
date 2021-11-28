00b5f989611b1a466a3ec14397c483c54ee5346d
bannzai <kingkong999yhirose@gmail.com>
2021-11-28 13:57:06 +0900

    :sparkles: defined modifier

--- src/swiftui/types/modifiers.ts
+++ src/swiftui/types/modifiers.ts
@@ -1,1 +1,27 @@
-export interface Modifier {}
+import { Color } from "./views";
+
+export interface Modifier {
+  readonly name: string;
+}
+
+export interface FrameModifier extends Modifier {
+  readonly name: "frame";
+  width?: number;
+  height?: number;
+  maxWidth?: "infinity";
+  maxHeight?: "infinity";
+}
+
+export interface PaddingModifier extends Modifier {
+  readonly name: "padding";
+
+  top?: number;
+  leading?: number;
+  bottom?: number;
+  trailing?: number;
+}
+
+export interface BackgroundModifier extends Modifier {
+  readonly name: "background";
+  view: Color;
+}
