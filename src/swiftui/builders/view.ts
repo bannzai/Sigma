b5d96ae525588b5b9daf663f4b6d6c1c8c6fc10e
bannzai <kingkong999yhirose@gmail.com>
2021-11-30 00:14:56 +0900

    :sparkles: fill all case
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

--- src/swiftui/builders/view.ts
+++ src/swiftui/builders/view.ts
@@ -1,9 +1,18 @@
-import { SwiftUIViewType, View } from "../types/views";
+import { SwiftUIViewType } from "../types/views";
 import { BuildContext } from "./context";
 import { walkToText } from "./text";
 
 export function walkToView(context: BuildContext, view: SwiftUIViewType) {
-  if (view.type === "Text") {
+  if (view.type === "VStack") {
+  } else if (view.type === "HStack") {
+  } else if (view.type === "ZStack") {
+  } else if (view.type === "Button") {
+  } else if (view.type === "Color") {
+  } else if (view.type === "Image") {
+  } else if (view.type === "Text") {
     walkToText(context, view);
+  } else if (view.type === "Divider") {
+  } else {
+    const _: never = view;
   }
 }
