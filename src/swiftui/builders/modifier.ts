083a4a54f717fb7603daf21bc85b920c26efdaa0
bannzai <kingkong999yhirose@gmail.com>
2021-11-29 17:00:38 +0900

    :sparkles: implement background modifier

--- src/swiftui/builders/modifier.ts
+++ src/swiftui/builders/modifier.ts
@@ -2,6 +2,7 @@ import { SwiftUIViewModifier } from "../types/modifiers";
 import { BuildContext } from "./context";
 import { walkToPadding } from "./padding";
 import { walkToFrame } from "./frame";
+import { walkToBackground } from "./background";
 
 export function walkToModifier(
   context: BuildContext,
@@ -12,6 +13,7 @@ export function walkToModifier(
   } else if (modifier.type === "padding") {
     walkToPadding(context, modifier);
   } else if (modifier.type === "background") {
+    walkToBackground(context, modifier);
   } else if (modifier.type === "overlay") {
   } else if (modifier.type === "stroke") {
   } else if (modifier.type === "clipShape") {
