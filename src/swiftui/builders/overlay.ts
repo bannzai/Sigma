ac2309b6017a9213cfb146200ffe2682f335a3e1
bannzai <kingkong999yhirose@gmail.com>
2021-11-29 21:30:02 +0900

    :sparkles: Prepare overlay
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

---
+++ src/swiftui/builders/overlay.ts
@@ -0,0 +1,12 @@
+import { OverlayModifier } from "../types/modifiers";
+import { BuildContext } from "./context";
+import { walkToShape } from "./shape";
+
+export function overlay(context: BuildContext, overlay: OverlayModifier) {
+  context.add(`.overlay(`, { withoutLineBreak: true });
+  context.disableIndent();
+  context.disableLineBreak();
+  walkToShape(context, overlay.shape);
+  context.enableIndent();
+  context.enableLineBreak();
+}
