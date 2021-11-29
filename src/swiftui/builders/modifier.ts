3cf613ecf5bf2f5829a7aa9a791139611f6d17e5
bannzai <kingkong999yhirose@gmail.com>
2021-11-29 16:16:14 +0900

    :sparkles: fill switch condition for modifier.type

--- src/swiftui/builders/modifier.ts
+++ src/swiftui/builders/modifier.ts
@@ -4,4 +4,17 @@ import { BuildContext } from "./context";
 export function walkToModifier(
   context: BuildContext,
   modifier: SwiftUIViewModifier
-) {}
+) {
+  if (modifier.type === "frame") {
+  } else if (modifier.type === "padding") {
+  } else if (modifier.type === "background") {
+  } else if (modifier.type === "overlay") {
+  } else if (modifier.type === "stroke") {
+  } else if (modifier.type === "clipShape") {
+  } else if (modifier.type === "mask") {
+  } else if (modifier.type === "cornerRadius") {
+  } else if (modifier.type === "position") {
+  } else {
+    const _: never = modifier;
+  }
+}
