8f18709fa3d949b30bbad175d10fc147f0fa3abf
bannzai <kingkong999yhirose@gmail.com>
2021-11-29 21:30:13 +0900

    :sparkles: implement walk to shape modifier
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

---
+++ src/swiftui/builders/shape.ts
@@ -0,0 +1,44 @@
+import { isSwiftUIViewShapeModifier } from "../types/shapeModifier";
+import { SwiftUIViewShape } from "../types/views";
+import { BuildContext } from "./context";
+import { walkToModifier } from "./modifier";
+import { walkToShapeModifier } from "./shapeModifier";
+
+export function walkToShape(context: BuildContext, shape: SwiftUIViewShape) {
+  if (shape.type === "Rectangle") {
+    context.add(`Rectangle()`);
+    context.nest();
+    shape.modifiers.forEach((e) => {
+      if (isSwiftUIViewShapeModifier(e)) {
+        walkToShapeModifier(context, e);
+      } else {
+        walkToModifier(context, e);
+      }
+    });
+    context.unnest();
+  } else if (shape.type === "RoundedRectangle") {
+    context.add(`RoundedRectangle(cornerRadius: ${shape.cornerRadius})`);
+    context.nest();
+    shape.modifiers.forEach((e) => {
+      if (isSwiftUIViewShapeModifier(e)) {
+        walkToShapeModifier(context, e);
+      } else {
+        walkToModifier(context, e);
+      }
+    });
+    context.unnest();
+  } else if (shape.type === "Ellipse") {
+    context.add(`Ellipse()`);
+    context.nest();
+    shape.modifiers.forEach((e) => {
+      if (isSwiftUIViewShapeModifier(e)) {
+        walkToShapeModifier(context, e);
+      } else {
+        walkToModifier(context, e);
+      }
+    });
+    context.unnest();
+  } else {
+    const _: never = shape;
+  }
+}
