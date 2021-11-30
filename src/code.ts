694ea37b00750abf4085641f0194b05b847c44a9
bannzai <kingkong999yhirose@gmail.com>
2021-11-30 22:29:39 +0900

    :recycle: refactor entrypoin

--- src/code.ts
+++ src/code.ts
@@ -1,24 +1,10 @@
-import * as assert from "assert";
-import { BuildContext } from "./swiftui/builders/context";
-import { walk as walkToSwiftUI } from "./swiftui/builders/entrypoint";
-import { SwiftUIContext } from "./swiftui/context";
-import { walk as walkToFigma } from "./swiftui/walks/walk";
+import { run } from "./run";
 
 const main = async () => {
   const root = figma.currentPage.selection[0];
   const code = run(root);
   console.log(code);
   figma.closePlugin();
-};
-
-export const run = (root: SceneNode): string => {
-  const figmaContext = new SwiftUIContext();
-  walkToFigma(figmaContext, root);
-  assert(figmaContext.root != null);
-
-  const buildContext = new BuildContext();
-  walkToSwiftUI(buildContext, root);
-  return buildContext.code;
 };
 
 main();
---
+++ src/run.ts
@@ -0,0 +1,15 @@
+import * as assert from "assert";
+import { BuildContext } from "./swiftui/builders/context";
+import { walk as walkToSwiftUI } from "./swiftui/builders/entrypoint";
+import { SwiftUIContext } from "./swiftui/context";
+import { walk as walkToFigma } from "./swiftui/walks/walk";
+
+export const run = (root: SceneNode): string => {
+  const figmaContext = new SwiftUIContext();
+  walkToFigma(figmaContext, root);
+  assert(figmaContext.root != null);
+
+  const buildContext = new BuildContext();
+  walkToSwiftUI(buildContext, root);
+  return buildContext.code;
+};
