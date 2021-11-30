092b4a5a0146acb4d2a0d6016dc71178f85d74b2
bannzai <kingkong999yhirose@gmail.com>
2021-11-30 22:29:01 +0900

    :recycle: fix entrypoint

--- src/code.ts
+++ src/code.ts
@@ -4,18 +4,21 @@ import { walk as walkToSwiftUI } from "./swiftui/builders/entrypoint";
 import { SwiftUIContext } from "./swiftui/context";
 import { walk as walkToFigma } from "./swiftui/walks/walk";
 
-const run = async () => {
+const main = async () => {
   const root = figma.currentPage.selection[0];
+  const code = run(root);
+  console.log(code);
+  figma.closePlugin();
+};
 
+export const run = (root: SceneNode): string => {
   const figmaContext = new SwiftUIContext();
   walkToFigma(figmaContext, root);
   assert(figmaContext.root != null);
 
   const buildContext = new BuildContext();
   walkToSwiftUI(buildContext, root);
-
-  console.log(buildContext.code);
-  figma.closePlugin();
+  return buildContext.code;
 };
 
-run();
+main();
