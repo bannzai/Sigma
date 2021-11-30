2d8ed785e834c5937e93297f15663386b9670067
bannzai <kingkong999yhirose@gmail.com>
2021-11-30 22:06:37 +0900

    :memo: walk each trees

--- src/code.ts
+++ src/code.ts
@@ -1,17 +1,27 @@
-import { FakeRootView, SwiftUIContext } from "./swiftui/context";
-import { walk } from "./swiftui/walks/walk";
+import * as assert from "assert";
+import { BuildContext } from "./swiftui/builders/context";
+import { walk as walkToSwiftUI } from "./swiftui/builders/entrypoint";
+import { isFakeRootView, SwiftUIContext } from "./swiftui/context";
+import { walk as walkToFigma } from "./swiftui/walks/walk";
 
 const run = async () => {
   const root = figma.currentPage.selection[0];
   const traversedContext = traversed(root);
-  print(traversedContext);
+  assert(!isFakeRootView(traversedContext.root));
+  const code = build(traversedContext.root);
   figma.closePlugin();
 };
 
 const traversed = (root: SceneNode): SwiftUIContext => {
   const context = new SwiftUIContext();
-  walk(context, root);
+  walkToFigma(context, root);
   return context;
+};
+
+const build = (root: { type: string }): string => {
+  const context = new BuildContext();
+  walkToSwiftUI(context, root);
+  return context.code;
 };
 
 const print = (context: SwiftUIContext) => {
