7856a86d4813463f894b5f712ec69a77cf05e923
bannzai <kingkong999yhirose@gmail.com>
2021-12-01 12:01:13 +0900

    :sparkles: add side effect

--- src/swiftui/context.ts
+++ src/swiftui/context.ts
@@ -4,6 +4,7 @@ import { ChildrenMixin, isContainerType, View } from "./types/views";
 export class SwiftUIContext {
   root!: { type: string; node: SceneNode | null };
   containerHistories: (View & ChildrenMixin)[] = [];
+  currentView: View | null = null;
 
   get container(): (View & ChildrenMixin) | null {
     if (this.containerHistories.length <= 0) {
@@ -15,18 +16,32 @@ export class SwiftUIContext {
     if (this.root == null) {
       this.root = container;
     }
+
     this.container?.children.push(container);
     this.containerHistories.push(container);
+
+    this.currentView = container;
   }
   unnestContainer(): (View & ChildrenMixin) | null {
-    return this.containerHistories.pop() ?? null;
+    const popped = this.containerHistories.pop();
+
+    if (this.containerHistories.length > 0) {
+      this.currentView =
+        this.containerHistories[this.containerHistories.length - 1];
+    } else {
+      this.currentView = null;
+    }
+
+    return popped ?? null;
   }
 
-  addChild(view: { type: string; node: SceneNode | null }) {
+  addChild(view: View) {
     if (this.root == null) {
       this.root = view;
     } else {
       this.container?.children.push(view);
     }
+
+    this.currentView = view;
   }
 }
