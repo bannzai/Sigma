3bd93e0c4b241a8daf255ae4d9835d9b049977f9
bannzai <kingkong999yhirose@gmail.com>
2021-12-06 10:05:48 +0900

    :recycle: add logging

--- src/reader/node/componentNode.ts
+++ src/reader/node/componentNode.ts
@@ -4,8 +4,10 @@ import { walkForPadding } from "../modifiers/padding";
 
 export function walkToComponent(context: FigmaContext, node: ComponentNode) {
   trace(`#walkToComponent`, context, node);
-  const { documentationLinks, remote } = node;
-  console.log(JSON.stringify({ documentationLinks, remote }));
+  const { documentationLinks, remote, variantProperties } = node;
+  console.log(
+    JSON.stringify({ documentationLinks, remote, variantProperties })
+  );
 
   if (documentationLinks.length === 0 || remote) {
     return;
