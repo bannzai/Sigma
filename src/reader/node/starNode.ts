c36dca9b83c5c18d8355e035921694aa1ce02db2
bannzai <kingkong999yhirose@gmail.com>
2021-12-07 05:59:07 +0900

    :memo: add switch condition

---
+++ src/reader/node/starNode.ts
@@ -0,0 +1,35 @@
+import { trace } from "../../util/tracer";
+import { FigmaContext } from "../context";
+import { walkForTextModifier } from "../modifiers/textModifier";
+import { adaptFrameModifierWithFrameNode } from "../modifiers/frame";
+import { Image, Text } from "../../types/views";
+
+export function walkToStar(context: FigmaContext, node: StarNode) {
+  trace(`#walkToText`, context, node);
+  const { fills, strokes } = node;
+
+  if (strokes.length > 0) {
+    const image: Image = {
+      type: "Image",
+      systemName: "star",
+      modifiers: [],
+      parent: context.container,
+      node: node,
+    };
+    context.addChild(image);
+  } else if (fills !== figma.mixed) {
+    const fill = fills[0];
+    if (fill.type === "SOLID") {
+      const image: Image = {
+        type: "Image",
+        systemName: "star.fill",
+        modifiers: [],
+        parent: context.container,
+        node: node,
+      };
+      context.addChild(image);
+    }
+  }
+
+  adaptFrameModifierWithFrameNode(context, context.findBy(node), node);
+}
