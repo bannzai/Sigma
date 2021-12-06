3c4a115aaeae87a50351a89df3a1dc7f355a0dc8
bannzai <kingkong999yhirose@gmail.com>
2021-12-06 10:03:20 +0900

    :recycle: add early return when documentation links is empty

--- src/reader/node/componentNode.ts
+++ src/reader/node/componentNode.ts
@@ -2,9 +2,13 @@ import { trace } from "../../util/tracer";
 import { FigmaContext } from "../context";
 import { walkForPadding } from "../modifiers/padding";
 
-
 export function walkToComponent(context: FigmaContext, node: ComponentNode) {
   trace(`#walkToComponent`, context, node);
+  const { documentationLinks } = node;
+
+  if (documentationLinks.length === 0) {
+    return;
+  }
 
   walkForPadding(context, context.findBy(node), node);
 }
