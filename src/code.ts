9ccbc2707613557e2e41c2bec8aa608570ef6946
bannzai <kingkong999yhirose@gmail.com>
2021-12-08 08:10:36 +0900

    show ui

--- src/code.ts
+++ src/code.ts
@@ -1,10 +1,14 @@
 import { run } from "./run";
 
+figma.showUI(__html__, { width: 480, height: 480 });
+
 const main = async () => {
   const root = figma.currentPage.selection[0];
   const code = run(root);
   console.log(code);
-  figma.closePlugin();
+  figma.ui.postMessage({
+    code,
+  });
 };
 
 main();
