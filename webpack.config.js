b41f4173f75b695899c2e2f5aef99124f6f52d93
bannzai <kingkong999yhirose@gmail.com>
2021-12-08 20:15:18 +0900

    :recycle: configure inline source plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env, argv) => ({
   entry: {
     ui: "./src/ui.tsx",
     code: "./src/code.ts",
   },
-  cache: false,
   mode: argv.mode === "production" ? "production" : "development",
   devtool: argv.mode === "production" ? false : "inline-source-map",
   module: {
@@ -31,16 +31,21 @@ module.exports = (env, argv) => ({
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "lib"),
+    // Workaround: https://github.com/DustinJackson/html-webpack-inline-source-plugin/issues/57#issuecomment-603218420
+    publicPath: "",
   },
   plugins: [
     new webpack.ProvidePlugin({
       process: "process/browser",
     }),
     new HtmlWebpackPlugin({
+      inject: true,
       template: "./src/ui.html",
       filename: "ui.html",
-      inlineSource: ".(js)$",
-      chunks: ["ui"],
+      inlineSource: ".(js|css)$",
+      cache: false,
+      minify: false,
     }),
+    new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
   ],
 });
