6d468100953966f2457ec97bd31a411863193840
bannzai <kingkong999yhirose@gmail.com>
2021-12-09 07:59:37 +0900

    :recycle: fix component

--- src/reader/node/componentNode.ts
+++ src/reader/node/componentNode.ts
@@ -1,18 +1,19 @@
 import { trace } from "../../util/tracer";
 import { FigmaContext } from "../context";
+import { traverse } from "../entrypoint";
 import { adaptModifier } from "../modifiers/adaptModifier";
 import { appendPadding } from "../modifiers/padding";
 
 export function walkToComponent(context: FigmaContext, node: ComponentNode) {
   trace(`#walkToComponent`, context, node);
-  const { documentationLinks, remote, variantProperties } = node;
-  console.log(
-    JSON.stringify({ documentationLinks, remote, variantProperties })
-  );
+  const { children, remote, variantProperties } = node;
+  console.log(JSON.stringify({ children, remote, variantProperties }));
 
   if (remote) {
     return;
   }
 
-  adaptModifier(context, context.findBy(node), node);
+  children.forEach((e) => {
+    traverse(context, e);
+  });
 }
