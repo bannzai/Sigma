d06d4eb8d19e1cecb36841b298997131b8050b4f
bannzai <kingkong999yhirose@gmail.com>
2021-12-11 05:13:44 +0900

    :memo: add type of tracer

--- src/builder/tracer.ts
+++ src/builder/tracer.ts
@@ -1,5 +1,6 @@
 import { ImageModifier } from "../types/imageModifier";
 import { Modifier } from "../types/modifiers";
+import { ShapeModifier } from "../types/shapeModifier";
 import { TextModifier } from "../types/textModifier";
 import { View } from "../types/views";
 import { BuildContext } from "./context";
@@ -7,7 +8,7 @@ import { BuildContext } from "./context";
 export function trace(
   prefix: string = "",
   context: BuildContext,
-  view: View | Modifier | ImageModifier | TextModifier
+  view: View | Modifier | ImageModifier | TextModifier | ShapeModifier
 ) {
   const { type } = view;
   console.log(`${prefix} ${JSON.stringify({ type })}`);
