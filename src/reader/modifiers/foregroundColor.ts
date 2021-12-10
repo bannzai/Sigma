573da8fdce919afa39a64b6cc2c21c96f3ed8069
bannzai <kingkong999yhirose@gmail.com>
2021-12-11 05:26:52 +0900

    :recycle: Remove unnecessary code

--- src/reader/modifiers/foregroundColor.ts
+++ src/reader/modifiers/foregroundColor.ts
@@ -32,36 +32,3 @@ export function appendForegroundColor(
     }
   }
 }
-
-/**
- Reference: https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/typography/
- 
- Swift Interface
- @frozen public struct Weight : Hashable {
-    public static let ultraLight: Font.Weight
-    public static let thin: Font.Weight
-    public static let light: Font.Weight
-    public static let regular: Font.Weight
-    public static let medium: Font.Weight
-    public static let semibold: Font.Weight
-    public static let bold: Font.Weight
-    public static let heavy: Font.Weight
-    public static let black: Font.Weight
- }
-*/
-
-function mappedFontWeight(fontName: FontName): NamedFontWeight | null {
-  const mapOfFigmaAndSwiftUIFontWeight: { [key: string]: NamedFontWeight } = {
-    thin: "ultraLight",
-    extralight: "thin",
-    light: "light",
-    regular: "regular",
-    medium: "medium",
-    semibold: "semibold",
-    bold: "bold",
-    extrabold: "heavy",
-    heavy: "heavy",
-    black: "black",
-  };
-  return mapOfFigmaAndSwiftUIFontWeight[fontName.style];
-}
