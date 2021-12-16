da5c02ea57a78a9158620de424830f24186e66a9
bannzai <kingkong999yhirose@gmail.com>
2021-12-16 21:56:51 +0900

    Fix async image tests

--- src/builder/view.ts
+++ src/builder/view.ts
@@ -46,19 +46,21 @@ export function buildView(context: BuildContext, view: SwiftUIViewType & View) {
   } else if (view.type === "Color") {
     context.add(`${mappedSwiftUIColor(view)}`);
   } else if (view.type === "Image") {
-    if (view.name != null) {
+    if (view.isAsyncImage) {
+      context.add(`image`);
+    } else if (view.name != null) {
       context.add(`Image("${view.name}")`);
     } else if (view.systemName != null) {
       context.add(`Image(systemName: "${view.systemName}")`);
-    } else if (view.isAsyncImage) {
-      context.add(`image`);
     }
   } else if (view.type === "AsyncImage") {
-    context.add("AsyncImage { phase in");
+    context.add(
+      `AsyncImage(url: URL(string: "https://picsum.photos/seed/picsum/200/300")!) { phase in`
+    );
     context.nestBlock(() => {
       context.add("switch phase {");
       context.nestBlock(() => {
-        context.add("case .success(let image)");
+        context.add("case .success(let image):");
         context.nestBlock(() => {
           buildView(context, view.image);
         });
