d3459f586c53fe7e8a27e43f5417c4f6b6aa109a
bannzai <kingkong999yhirose@gmail.com>
2021-12-16 20:33:18 +0900

    :recycle: replace to create image function

--- src/reader/view/image.ts
+++ src/reader/view/image.ts
@@ -1,17 +1,11 @@
 import { FigmaContext } from "../context";
 import { Image } from "../../types/views";
-import { appendForegroundColor } from "../modifiers/foregroundColor";
-import { appendFixedFrame } from "../modifiers/frame";
 import { trace } from "../tracer";
 
 export type ImageNode = DefaultShapeMixin & SceneNode;
 
-export function walkForImage(
-  context: FigmaContext,
-  fill: ImagePaint,
-  node: ImageNode
-) {
-  trace("#walkForImage", context, node);
+export function createImage(context: FigmaContext, node: ImageNode): Image {
+  trace("createImage", context, node);
   const { name } = node;
 
   let image: Image;
@@ -31,11 +25,5 @@ export function walkForImage(
       node: node,
     };
   }
-  context.addChild(image);
-
-  if (fill.scaleMode === "FIT") {
-    image.modifiers.push({ type: "resizable" });
-    appendFixedFrame(context, image, node);
-  }
-  appendForegroundColor(context, node, image);
+  return image;
 }
