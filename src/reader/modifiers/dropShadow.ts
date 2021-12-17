fa37dfaf04a58ba4123417a97ef1a0fe648cba0f
bannzai <kingkong999yhirose@gmail.com>
2021-12-17 23:34:52 +0900

    Add append drop shadow method

---
+++ src/reader/modifiers/dropShadow.ts
@@ -0,0 +1,41 @@
+import {
+  CompositingGroupModifier,
+  ShadowModifier,
+} from "../../types/modifiers";
+import { Color, View } from "../../types/views";
+import { FigmaContext } from "../context";
+import { trace } from "../tracer";
+
+export function appendDropShadow(
+  context: FigmaContext,
+  view: View,
+  node: SceneNode & BlendMixin
+) {
+  for (const effect of node.effects) {
+    if (effect.type === "DROP_SHADOW") {
+      const { color, radius, offset } = effect;
+      const { x, y } = offset;
+      trace("#appendDropShadow", context, node, { color, radius, x, y });
+
+      const compositingGroupModifier: CompositingGroupModifier = {
+        type: "compositingGroup",
+      };
+      view.modifiers.push(compositingGroupModifier);
+
+      const shadow: ShadowModifier = {
+        type: "shadow",
+        color: {
+          type: "Color",
+          red: color.r,
+          green: color.g,
+          blue: color.b,
+          opacity: color.a,
+        },
+        radius,
+        x,
+        y,
+      };
+      view.modifiers.push(shadow);
+    }
+  }
+}
