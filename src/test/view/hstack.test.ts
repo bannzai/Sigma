586f3c3a9cabbb99d7f401990c2c30105c4d2a43
bannzai <kingkong999yhirose@gmail.com>
2021-12-26 17:48:28 +0900

    :memo: add comment

--- src/test/view/hstack.test.ts
+++ src/test/view/hstack.test.ts
@@ -7,7 +7,8 @@ describe("#HStack", () => {
     simulateErrors: true,
     isWithoutTimeout: false,
   });
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars for some reason, need to override this for figma.mixed to work
+  // eslint-disable-next-line
+  // @ts-ignore
   global.figma = figma;
 
   test("it is besically pattern. HStack has three text", async () => {
--- src/test/view/zstack.test.ts
+++ src/test/view/zstack.test.ts
@@ -7,7 +7,8 @@ describe("#ZStack", () => {
     simulateErrors: true,
     isWithoutTimeout: false,
   });
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars for some reason, need to override this for figma.mixed to work
+  // eslint-disable-next-line
+  // @ts-ignore
   global.figma = figma;
   jest.mock(
     "../../../node_modules/@figma/plugin-typings/plugin-api.d.ts",
