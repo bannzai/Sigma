4617e25f94fd605cc50c11bad4598ee42a8dea26
bannzai <kingkong999yhirose@gmail.com>
2021-12-26 17:29:52 +0900

    :recycle: special walk method for read grid context

--- src/reader/view/grid.ts
+++ src/reader/view/grid.ts
@@ -1,38 +1,117 @@
-import { LazyHGrid, LazyVGrid } from "../../types/views";
+import { LazyHGrid, LazyVGrid, Section } from "../../types/views";
 import { FigmaContext } from "../context";
+import { traverse } from "../entrypoint";
 
 export function walkForVGridChildren(
-  _context: FigmaContext,
+  context: FigmaContext,
   gridNode: FrameNode,
   grid: LazyVGrid
 ) {
-  let maximumGridItemCount = 0;
-  for (const child of gridNode.children) {
-    if (child.type === "FRAME" && child.layoutMode === "HORIZONTAL") {
-      maximumGridItemCount = Math.max(
-        maximumGridItemCount,
-        child.children.length
-      );
-    }
-  }
+  gridNode.children.forEach((child, index) => {
+    if (child.type === "FRAME" && child.children.length > 0) {
+      if (child.layoutMode === "HORIZONTAL") {
+        grid.maximumGridItemCount = Math.max(
+          grid.maximumGridItemCount,
+          child.children.length
+        );
 
-  grid.maximumGridItemCount = maximumGridItemCount;
+        child.children.forEach((gridChild) => {
+          traverse(context, gridChild);
+        });
+      } else if (child.layoutMode === "VERTICAL") {
+        if (index === 0) {
+          const section: Section = {
+            type: "Section",
+            modifiers: [],
+            node: child,
+            children: [],
+          };
+          context.nestContainer(section);
+          child.children.forEach((sectionChild) => {
+            traverse(context, sectionChild);
+          });
+          context.unnestContainer();
+
+          const header = section.children.shift();
+          if (header != null) {
+            section.header = header;
+          }
+        } else if (index === gridNode.children.length - 1) {
+          const section: Section = {
+            type: "Section",
+            modifiers: [],
+            node: child,
+            children: [],
+          };
+          context.nestContainer(section);
+          child.children.forEach((sectionChild) => {
+            traverse(context, sectionChild);
+          });
+          context.unnestContainer();
+
+          const footer = section.children.pop();
+          if (footer != null) {
+            section.footer = footer;
+          }
+        }
+      }
+    }
+  });
 }
 
 export function walkForHGridChildren(
-  _context: FigmaContext,
+  context: FigmaContext,
   gridNode: FrameNode,
   grid: LazyHGrid
 ) {
-  let maximumGridItemCount = 0;
-  for (const child of gridNode.children) {
-    if (child.type === "FRAME" && child.layoutMode === "VERTICAL") {
-      maximumGridItemCount = Math.max(
-        maximumGridItemCount,
-        child.children.length
-      );
-    }
-  }
+  gridNode.children.forEach((child, index) => {
+    if (child.type === "FRAME" && child.children.length > 0) {
+      if (child.layoutMode === "VERTICAL") {
+        grid.maximumGridItemCount = Math.max(
+          grid.maximumGridItemCount,
+          child.children.length
+        );
 
-  grid.maximumGridItemCount = maximumGridItemCount;
+        child.children.forEach((gridChild) => {
+          traverse(context, gridChild);
+        });
+      } else if (child.layoutMode === "HORIZONTAL") {
+        if (index === 0) {
+          const section: Section = {
+            type: "Section",
+            modifiers: [],
+            node: child,
+            children: [],
+          };
+          context.nestContainer(section);
+          child.children.forEach((sectionChild) => {
+            traverse(context, sectionChild);
+          });
+          context.unnestContainer();
+
+          const header = section.children.shift();
+          if (header != null) {
+            section.header = header;
+          }
+        } else if (index === gridNode.children.length - 1) {
+          const section: Section = {
+            type: "Section",
+            modifiers: [],
+            node: child,
+            children: [],
+          };
+          context.nestContainer(section);
+          child.children.forEach((sectionChild) => {
+            traverse(context, sectionChild);
+          });
+          context.unnestContainer();
+
+          const footer = section.children.pop();
+          if (footer != null) {
+            section.footer = footer;
+          }
+        }
+      }
+    }
+  });
 }
