9735866200c239916b28cd3c017f0656430fdcc7
bannzai <kingkong999yhirose@gmail.com>
2021-12-27 19:19:28 +0900

    :recycle: Replace to createHStack

--- src/test/view/vgrid.test.ts
+++ src/test/view/vgrid.test.ts
@@ -1,24 +1,6 @@
-import { createText } from "../utility/utility";
+import { createText, createHStack } from "../utility/utility";
 import { createFigma } from "figma-api-stub";
 import { testRun } from "../../run";
-
-function hstack(): FrameNode {
-  const hstack = figma.createFrame();
-  hstack.name = "SwiftUI::Grid";
-  hstack.layoutMode = "HORIZONTAL";
-  hstack.counterAxisAlignItems = "MIN";
-  hstack.paddingLeft = 0;
-  hstack.paddingTop = 0;
-  hstack.paddingRight = 0;
-  hstack.paddingBottom = 0;
-  hstack.itemSpacing = 10;
-  hstack.appendChild(createText("1"));
-  hstack.appendChild(createText("2"));
-  hstack.appendChild(createText("3"));
-  hstack.strokes = [];
-  hstack.effects = [];
-  return hstack;
-}
 
 describe("#LazyVGrid", () => {
   const figma = createFigma({
@@ -56,7 +38,7 @@ describe("#LazyVGrid", () => {
     vgrid.paddingRight = 0;
     vgrid.paddingBottom = 0;
     vgrid.itemSpacing = 10;
-    vgrid.appendChild(hstack());
+    vgrid.appendChild(createHStack());
     vgrid.strokes = [];
     vgrid.effects = [];
 
