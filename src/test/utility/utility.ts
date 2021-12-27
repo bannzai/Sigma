286f1a00cbe8f3b042bc65c145e173d23acbf778
bannzai <kingkong999yhirose@gmail.com>
2021-12-27 19:19:06 +0900

    Add utility method

--- src/test/utility/utility.ts
+++ src/test/utility/utility.ts
@@ -10,3 +10,21 @@ export function createText(text: string): TextNode {
   node.effects = [];
   return node;
 }
+
+export function createHStack(): FrameNode {
+  const hstack = figma.createFrame();
+  hstack.name = "SwiftUI::Grid";
+  hstack.layoutMode = "HORIZONTAL";
+  hstack.counterAxisAlignItems = "MIN";
+  hstack.paddingLeft = 0;
+  hstack.paddingTop = 0;
+  hstack.paddingRight = 0;
+  hstack.paddingBottom = 0;
+  hstack.itemSpacing = 10;
+  hstack.appendChild(createText("1"));
+  hstack.appendChild(createText("2"));
+  hstack.appendChild(createText("3"));
+  hstack.strokes = [];
+  hstack.effects = [];
+  return hstack;
+}
